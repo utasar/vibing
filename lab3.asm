e797b11a63fd3415270c77a81eee5ff81beb7f3c
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2025-09-26 12:36:30 -0400

    Create lab3.asm

---
+++ lab3.asm
@@ -0,0 +1,57 @@
+.ORIG x3000
+; = = = = = = = = = = = = = = = = =
+
+; MAIN CODE HERE
+
+HALT
+; - - - - - - - - - - - - - - - - -
+
+; MAIN VARS
+
+; = = = = = = = = = = = = = = = = =
+
+; = = = = = = = = = = = = = = = = =
+DISPLAY
+
+; DISPLAY CODE HERE
+
+RET
+; - - - - - - - - - - - - - - - - -
+; DISPLAY VARS
+
+; = = = = = = = = = = = = = = = = =
+
+; = = = = = = = = = = = = = = = = =
+GETNUM
+
+; GETNUM CODE HERE
+
+RET
+; - - - - - - - - - - - - - - - - -
+; GETNUM VARS
+
+; = = = = = = = = = = = = = = = = =
+
+; = = = = = = = = = = = = = = = = =
+GETOP
+
+; GETOP CODE HERE
+
+RET
+; - - - - - - - - - - - - - - - - -
+; GETOP VARS
+
+; = = = = = = = = = = = = = = = = =
+
+; = = = = = = = = = = = = = = = = =
+CALC
+
+; CALC CODE HERE
+
+RET
+; - - - - - - - - - - - - - - - - -
+; CALC VARS
+
+; = = = = = = = = = = = = = = = = =
+
+.END
